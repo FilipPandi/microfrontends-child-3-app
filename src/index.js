517b3730060ad319140004737aa3e74ac4fe4fa5
filip <filip.pandi@inovatrend.com>
2023-04-17 16:01:45 +0200

    Fix mounting/unmounting

--- src/index.js
+++ src/index.js
@@ -2,22 +2,49 @@ import React from 'react';
 import './index.css';
 import App from './App';
 import * as ReactDOMClient from 'react-dom/client';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
 
+let isMounted = false;
 let root = null;
 let container = null;
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App/>,
+    }
+]);
+
 window.renderChild3 = (containerId) => {
     container = document.getElementById(containerId);
-    root = ReactDOMClient.createRoot(container);
 
-    root.render(<App/>);
+    if (isMounted === false) {
+        root = ReactDOMClient.createRoot(container);
+
+        root.render(
+            <React.StrictMode>
+                <RouterProvider router={router}/>
+            </React.StrictMode>);
+    }
+
+    isMounted = true;
+
+    console.log("Application 3 - Private - is mounted.");
 };
 
 window.unmountChild3 = () => {
-    root.unmount(container);
+    if (isMounted === true) {
+        root.unmount(container);
+        isMounted = false;
+
+        console.log("Application 3 - Private - unmounting previous...")
+    }
 };
 
 if (!document.getElementById('Child3-container')) {
     const container = document.getElementById('root');
     const root = ReactDOMClient.createRoot(container);
-    root.render(<App/>);
+    root.render(
+        <React.StrictMode>
+            <RouterProvider router={router}/>
+        </React.StrictMode>);
 }
