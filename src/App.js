a2e92e16b4f277e0935216c9f4ae4dab93f5b1ad
filip <filip.pandi@inovatrend.com>
2023-04-18 10:31:39 +0200

    Add routing to root app.

--- src/App.js
+++ src/App.js
@@ -4,11 +4,16 @@ import "primereact/resources/themes/lara-light-indigo/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
 import './App.css'
+import {Button} from "primereact/button";
 
 function App() {
     return (
         <div style={{margin: '5%'}}>
             <Panel header="Ovo je Privatni dio" className="custom-panel">
+                <h2><a className={"link"} href="http://localhost:3000/"><Button severity="secondary"
+                                                                                icon="pi pi-desktop"
+                                                                                label={"Root"}/></a></h2>
+                <br/>
                 PRIVATNI dio!
             </Panel>
         </div>
